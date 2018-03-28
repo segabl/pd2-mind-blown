bb42c2cd575ffa197b07817faa537e93a0379627
Sebastian Gabl <sebastian.gabl@gmx.at>
2018-03-28 12:39:25 +0200

    Rework to chain effect

--- mod.lua
+++ mod.lua
@@ -2,14 +2,20 @@ Hooks:Add("LocalizationManagerPostInit", "LocalizationManagerPostInitMindBlown",
 
   loc:add_localized_strings({
     menu_sniper_graze_damage = "Mind Blown",
-    menu_sniper_graze_damage_desc = "BASIC: ##4 points##\nScoring a headshot with a Sniper Rifle deals up to ##50%## of the damage in a ##3m## radius around the bullet impact.\n\nACE: ##8 points##\nThe effect radius is increased by ##65%##.\nScoring a killing headshot deals up to ##100%## of the damage in the effect radius."
+    menu_sniper_graze_damage_desc = "BASIC: ##4 points##\nScoring a headshot with a Sniper Rifle deals ##40%## of the damage to the closest enemy in a ##5m## radius.\n\nThe effect propagates from every additional enemy hit, with subsequent damage being capped by the damage dealt to that enemy.\n\nACE: ##8 points##\nScoring a killing headshot deals ##100%## of the damage to the closest enemy in the effect radius."
   })
 
 end)
 
 if RequiredScript == "lib/managers/player/snipergrazedamage" then
 
-  local brush = Draw:brush(Color(0.2, 1, 0, 0), 1)
+  local TRAIL_EFFECT = Idstring("effects/particles/weapons/sniper_trail")
+  local idstr_trail = Idstring("trail")
+  local idstr_simulator_length = Idstring("simulator_length")
+  local idstr_size = Idstring("size")
+  local trail_length = World:effect_manager():get_initial_simulator_var_vector2(TRAIL_EFFECT, idstr_trail, idstr_simulator_length, idstr_size)
+
+  local brush = Draw:brush(Color(0.1, 1, 0, 0), 2)
   function SniperGrazeDamage:on_weapon_fired(weapon_unit, result)
     if not alive(weapon_unit) or not weapon_unit:base():is_category("snp") or weapon_unit ~= managers.player:equipped_weapon_unit() or not result.hit_enemy then
       return
@@ -45,29 +51,65 @@ if RequiredScript == "lib/managers/player/snipergrazedamage" then
     
     local radius = upgrade_value.radius
     for _, hit in pairs(hit_enemies) do
-      --brush:sphere(hit.position, radius)
-      local hit_units = World:find_units_quick("sphere", hit.position, radius, enemy_mask)
-      for _, unit in ipairs(hit_units) do
-        if not hit_enemies[unit:key()] then
-          local hit_pos = Vector3()
-          mvector3.set(hit_pos, unit:movement():m_head_pos())
-          local percentage = 1 - math.min(1, math.max(0, (mvector3.distance(hit.position, hit_pos) - 100) / radius))
-          if World:raycast("ray", hit.position, hit_pos, "slot_mask", geometry_mask) then
-            percentage = percentage - 0.5
-          end
-          if percentage > 0 then
-            unit:character_damage():damage_simple({
-              variant = "graze",
-              damage = percentage * hit.damage,
-              attacker_unit = player_unit,
-              pos = hit_pos,
-              attack_dir = hit.position - hit_pos
-            })
+      self:find_closest_hit(hit, hit_enemies, radius, enemy_mask, geometry_mask, player_unit)
+    end
+    
+  end
+
+  function SniperGrazeDamage:find_closest_hit(hit, hit_enemies, radius, enemy_mask, geometry_mask, player_unit)
+    local hit_units = World:find_units_quick("sphere", hit.position, radius, enemy_mask)
+    local closest
+    local closest_d_sq
+    for _, unit in ipairs(hit_units) do
+      if not hit_enemies[unit:key()] then
+        local d_s = mvector3.distance_sq(hit.position, unit:movement():m_head_pos())
+        if not closest_d_sq or d_s < closest_d_sq then
+          if not World:raycast("ray", hit.position, unit:movement():m_head_pos(), "slot_mask", geometry_mask) then
+            closest = unit
+            closest_d_sq = d_s
           end
         end
       end
     end
-    
+    if closest then
+      local hit_pos = Vector3()
+      mvector3.set(hit_pos, closest:movement():m_head_pos())
+
+      --brush:cylinder(hit.position, hit_pos, 5)
+      local trail = World:effect_manager():spawn({
+        effect = Idstring("effects/particles/weapons/sniper_trail"),
+        position = hit.position,
+        normal = hit_pos - hit.position
+      })
+      mvector3.set_y(trail_length, math.sqrt(closest_d_sq))
+      World:effect_manager():set_simulator_var_vector2(trail, idstr_trail, idstr_simulator_length, idstr_size, trail_length)
+
+      hit_enemies[closest:key()] = data
+
+      DelayedCalls:Add("grazehit" .. tostring(closest), 0.1, function ()
+        if not alive(closest) or not alive(player_unit) then
+          return
+        end
+        
+        local result = closest:character_damage():damage_simple({
+          variant = "graze",
+          damage = hit.damage,
+          attacker_unit = player_unit,
+          pos = hit_pos,
+          attack_dir = hit_pos - hit.position
+        })
+
+        data = {
+          unit = closest,
+          position = hit_pos,
+          damage = result and result.damage or hit.damage
+        }
+      
+        self:find_closest_hit(data, hit_enemies, radius, enemy_mask, geometry_mask, player_unit)
+      end)
+
+    end
+  
   end
 
 end
@@ -80,13 +122,13 @@ if RequiredScript == "lib/tweak_data/upgradestweakdata" then
     
     self.values.snp.graze_damage = {
       {
-        radius = 300,
-        damage_factor = 0.5,
-        damage_factor_headshot = 0.5
+        radius = 500,
+        damage_factor = 0.4,
+        damage_factor_headshot = 0.4
       },
       {
         radius = 500,
-        damage_factor = 0.5,
+        damage_factor = 0.4,
         damage_factor_headshot = 1
       }
     }
