e23b1c40a55a30857aad48384917870da8b8a85c
Sebastian Gabl <sebastian.gabl@gmx.at>
2018-03-29 12:09:10 +0200

    Fix hits exceeding the limit

--- mod.lua
+++ mod.lua
@@ -15,7 +15,6 @@ if RequiredScript == "lib/managers/player/snipergrazedamage" then
   local idstr_size = Idstring("size")
   local trail_length
 
-  local brush = Draw:brush(Color(0.1, 1, 0, 0), 2)
   function SniperGrazeDamage:on_weapon_fired(weapon_unit, result)
     if not alive(weapon_unit) or not weapon_unit:base():is_category("snp") or weapon_unit ~= managers.player:equipped_weapon_unit() or not result.hit_enemy then
       return
@@ -32,6 +31,7 @@ if RequiredScript == "lib/managers/player/snipergrazedamage" then
     local enemy_mask = managers.slot:get_mask("enemies")
     local geometry_mask = managers.slot:get_mask("world_geometry")
     local hit_enemies = {}
+    local ignored_enemies = {}
 
     for _, hit in ipairs(result.rays) do
       local is_turret = hit.unit:in_slot(sentry_mask)
@@ -41,22 +41,23 @@ if RequiredScript == "lib/managers/player/snipergrazedamage" then
       local attack_data = result and result.attack_data
       if attack_data and attack_data.headshot and not is_turret and not is_ally then
         local multiplier = (result.type == "death" or result.type == "healed") and upgrade_value.damage_factor_kill or upgrade_value.damage_factor
-        hit_enemies[hit.unit:key()] = {
-          unit = hit.unit,
+        local key = hit.unit:key()
+        hit_enemies[key] = {
           position = hit.position,
           damage = attack_data.damage * multiplier
         }
+        ignored_enemies[key] = true
       end
     end
     
     local radius = upgrade_value.radius
     for _, hit in pairs(hit_enemies) do
-      self:find_closest_hit(hit, hit_enemies, upgrade_value, enemy_mask, geometry_mask, player_unit, upgrade_value.times)
+      self:find_closest_hit(hit, ignored_enemies, upgrade_value, enemy_mask, geometry_mask, player_unit, upgrade_value.times)
     end
     
   end
 
-  function SniperGrazeDamage:find_closest_hit(hit, hit_enemies, upgrade_value, enemy_mask, geometry_mask, player_unit, times)
+  function SniperGrazeDamage:find_closest_hit(hit, ignored_enemies, upgrade_value, enemy_mask, geometry_mask, player_unit, times)
     if times <= 0 then
       return
     end
@@ -65,7 +66,7 @@ if RequiredScript == "lib/managers/player/snipergrazedamage" then
     local closest
     local closest_d_sq
     for _, unit in ipairs(hit_units) do
-      if not hit_enemies[unit:key()] then
+      if not ignored_enemies[unit:key()] then
         local d_s = mvector3.distance_sq(hit.position, unit:movement():m_head_pos())
         if not closest_d_sq or d_s < closest_d_sq then
           if not World:raycast("ray", hit.position, unit:movement():m_head_pos(), "slot_mask", geometry_mask) then
@@ -76,10 +77,11 @@ if RequiredScript == "lib/managers/player/snipergrazedamage" then
       end
     end
     if closest then
+      ignored_enemies[closest:key()] = true
+
       local hit_pos = Vector3()
       mvector3.set(hit_pos, closest:movement():m_head_pos())
 
-      --brush:cylinder(hit.position, hit_pos, 5)
       if not trail_length then
         trail_length = World:effect_manager():get_initial_simulator_var_vector2(TRAIL_EFFECT, idstr_trail, idstr_simulator_length, idstr_size)
       end
@@ -90,8 +92,6 @@ if RequiredScript == "lib/managers/player/snipergrazedamage" then
       })
       mvector3.set_y(trail_length, math.sqrt(closest_d_sq))
       World:effect_manager():set_simulator_var_vector2(trail, idstr_trail, idstr_simulator_length, idstr_size, trail_length)
-
-      hit_enemies[closest:key()] = data
 
       DelayedCalls:Add("grazehit" .. tostring(closest), 0.05, function ()
         if not alive(closest) or not alive(player_unit) then
@@ -106,13 +106,12 @@ if RequiredScript == "lib/managers/player/snipergrazedamage" then
           attack_dir = hit_pos - hit.position
         })
 
-        data = {
-          unit = closest,
+        hit = {
           position = hit_pos,
           damage = result and result.damage or hit.damage
         }
       
-        self:find_closest_hit(data, hit_enemies, upgrade_value, enemy_mask, geometry_mask, player_unit, times - 1)
+        self:find_closest_hit(hit, ignored_enemies, upgrade_value, enemy_mask, geometry_mask, player_unit, times - 1)
       end)
 
     end
